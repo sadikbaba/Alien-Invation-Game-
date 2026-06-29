8d4c39920bc931e56e460301997cb48252b86213
Abubakar Baba <4821saddikhady@gmail.com>
2026-06-30 01:09:05 +0300

    added everything

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import settings
+from ship import Ship
 
 
 def run_game():
@@ -11,7 +12,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
-
+    
+    # Make a ship
+    ship = Ship(screen)                                                                             
    
 
     # start the main loop for the game
@@ -23,6 +26,7 @@ def run_game():
                 sys.exit()
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
