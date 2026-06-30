d10353d397a4bd170bfce0aa74aa3390e4757341
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-01 02:14:42 +0300

    updated

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -30,8 +30,7 @@ def run_game():
         ship.update()
 
         gf.update_bullets(bullets)
-
-        gf.update_aliens(aliens)
+        gf.update_aliens(ai_settings, aliens)
         # clock.tick(390)
 
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
