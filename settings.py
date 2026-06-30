92ca859f078a7e6924dc1d73f78d3f21c7516ad8
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-01 01:46:10 +0300

    added more settings

--- settings.py
+++ settings.py
@@ -18,3 +18,7 @@ class settings:
 
         # Alien settings
         self.alien_speed_factor = 1
+
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
