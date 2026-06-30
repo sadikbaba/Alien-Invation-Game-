e90f27724457ded4acddd98fbfc62d1c05be7ee6
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-01 01:34:21 +0300

    settings

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class settings:
     def __init__(self):
         """initialize the game's settings"""
         # screen settings
-        self.screen_width = 1100
+        self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
@@ -15,3 +15,6 @@ class settings:
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed_factor = 1
